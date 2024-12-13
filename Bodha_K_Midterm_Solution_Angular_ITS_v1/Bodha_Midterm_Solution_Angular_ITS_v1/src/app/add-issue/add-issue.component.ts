e398c9c2213dbf33cd9342f8024f7813d6461866
Bodha17 <Bodha@mail.com>
2024-12-13 15:06:35 +0530

    Submitted

---
+++ Bodha_K_Midterm_Solution_Angular_ITS_v1/Bodha_Midterm_Solution_Angular_ITS_v1/src/app/add-issue/add-issue.component.ts
@@ -0,0 +1,56 @@
+
+ 
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IssueService } from '../services/issue.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-add-issue',
+  templateUrl: './add-issue.component.html',
+  styleUrl: './add-issue.component.css'
+})
+export class AddIssueComponent implements OnInit {
+  addIssueForm: FormGroup;
+  id: number;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private issueService: IssueService) { }
+
+  ngOnInit(): void {
+    this.addIssueForm = this.formBuilder.group({
+      issueId: [''],
+      title: [''],
+      description: [''],
+      priority: [''],
+      status: [''],
+      asignee: [''],
+      date: [''],
+      id: ['']
+    });
+
+   
+    this.addIssueForm.get('issueId').valueChanges.subscribe(value => {
+      this.addIssueForm.patchValue({
+        id: value
+      }, { emitEvent: false });
+    });
+  }
+
+  onSubmit() {
+
+    const formValue = this.addIssueForm.value;
+    formValue.id = formValue.issueId;
+
+    this.issueService.createIssue(formValue).subscribe(data => {
+      Swal.fire({
+        icon: "success",
+        title: "",
+        text: "Issue Created",
+  });
+      this.router.navigate(['/Issues']);
+    });
+  }
+}
+
+ 
---
+++ Bodha_K_Midterm_Solution_Angular_ITS_v1/Bodha_Midterm_Solution_Angular_ITS_v1/src/app/update-issue/update-issue.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Issue } from '../models/issues.model';
+import { IssueService } from '../services/issue.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-update-issue',
+  templateUrl: './update-issue.component.html',
+  styleUrl: './update-issue.component.css'
+})
+export class UpdateIssueComponent implements OnInit {
+  id: number = 0;
+  issue: Issue = new Issue();
+
+  constructor(
+    private issueService: IssueService, 
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['issueId'];
+    this.issue = new Issue();
+    this.issueService.getIssueByID(this.id)
+      .subscribe(
+        searchIssue => {
+          this.issue = searchIssue;
+        },
+        error => console.log(error)
+      );
+  }
+
+  updateIssue(): void {
+    this.issueService.updateIssue(this.id, this.issue)
+      .subscribe(
+        updatedIssue => {
+          console.log(updatedIssue);
+        },
+        error => console.log(error)
+      );
+    this.router.navigate(['/Issues']);
+    Swal.fire({
+      icon: "success",
+      title: "",
+      text: "Issue Updated",
+  });
+  }
+} 
